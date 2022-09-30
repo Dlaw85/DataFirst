a0ece0a15bdec5d1daf0fe9b665f5e9af9e2908e
unknown <eyenghed@gmail.com>
2022-09-30 11:58:06 +0100

    latest commit

---
+++ Models/UserDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataBaseFApproach.Models
+{
+    public partial class UserDetail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? NickName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public string Detail { get; set; } = null!;
+    }
+}
---
+++ Models/WellaDbaseContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DataBaseFApproach.Models
+{
+    public partial class WellaDbaseContext : DbContext
+    {
+        public WellaDbaseContext()
+        {
+        }
+
+        public WellaDbaseContext(DbContextOptions<WellaDbaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<UserDetail> UserDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-BO6P2GQ\\SQLEXPRESS;Database=WellaDbase;Trusted_Connection=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserDetail>(entity =>
+            {
+                entity.ToTable("UserDetail");
+
+                entity.Property(e => e.Detail).HasMaxLength(50);
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.NickName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
